bebfdac44a9cd6e45fbca136e8c5219ec28bacb5
nordberaht <northberaht@gmail.com>
2023-01-14 21:51:13 +0100

    Final layout and responsivness

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,25 +1,32 @@
 import * as L from 'leaflet';
 
-const header = document.querySelector('.entry_page');
-const headerButton = document.querySelector('.btn-entry_page');
+// SELECTORS
+const header = document.querySelector('.entry-page');
+const headerButton = document.querySelector('.btn_entry-page');
 
 const showListButton = document.querySelector('.btn--aside');
-const allCountries = document.querySelector('.all_countries');
+const allCountries = document.querySelector('.all-countries');
 
 const mapDiv = document.querySelector('#map');
 
+// --------------FOR ENTRY PAGE--------------
 headerButton.addEventListener('click', () => {
   header.classList.add('hide');
   mapDiv.classList.remove('hide');
 });
 
+// --------------FOR ASIDE SECTION--------------
 showListButton.addEventListener('click', () => {
   allCountries.classList.toggle('slide');
 });
 
+// --------------COUNTRIES--------------
+
 fetch('https://restcountries.com/v3.1/all')
   .then((response) => response.json())
   .then((body) => console.log(body[0]));
+
+// --------------MAP--------------
 
 var map = L.map('map').setView([52.254294, 19.433056], 5);
 
@@ -36,3 +43,18 @@ L.marker([52.254294, 19.433056])
   .addTo(map)
   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
   .openPopup();
+
+// --------------WEATHER API--------------
+//  https://openweathermap.org/current#data
+let weather = {
+  apiKey: 'f92e96d37e2400c229247a58bb6f0f0f',
+  fetchWeather(city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((body) => console.log(body));
+  },
+};
+
+weather.fetchWeather('jelenia góra');
